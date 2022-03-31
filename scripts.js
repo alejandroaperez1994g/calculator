a0d96b7d88768fd40168360e4f3811fe02e627c9
Julio Macias Caldera <thebigjyt@gmail.com>
2022-03-31 14:25:17 +0200

    topbuttons js

--- scripts.js
+++ scripts.js
@@ -10,6 +10,13 @@ let data_clear_all = document.querySelector("[data-clear-all]");
 let data_clear = document.querySelector("[data-clear]");
 let data_equals = document.querySelector("[data-equal]");
 let sign_button = document.querySelector("[data-sign]");
+let data_point = document.querySelector('[data-pointer]');
+let checkbox = document.querySelector('#checkbox');
+
+let main = document.querySelector('.main');
+let main_top = document.querySelector('.main-top');
+let output = document.querySelector('.output');
+let check_label = document.querySelector('#check-label')
 
 Array.from(number_button).forEach((button) => {
   button.addEventListener("click", () => {
@@ -44,7 +51,6 @@ data_clear.addEventListener("click", () => {
 });
 
 data_clear_all.addEventListener("click", () => {
-  console.log(previousOperand, currentOperand, operation);
 
   previousOperand = "";
   currentOperand = "";
@@ -53,6 +59,72 @@ data_clear_all.addEventListener("click", () => {
   updateDisplay();
   previousElementDisplay.textContent = "";
 });
+
+sign_button.addEventListener('click', () =>{
+
+  if(currentOperand < 0){
+    currentOperand = currentOperand * -1
+  }else if(currentOperand > 0){
+    currentOperand = currentOperand * -1
+  }
+
+  updateDisplay();
+  
+})
+
+data_point.addEventListener('click', () =>{
+  console.log(data_point.TEXT_NODE)
+})
+
+checkbox.addEventListener('click', () =>{
+  if(checkbox.checked == true){
+    main.style.backgroundColor = '#2E3851';
+    main_top.style.backgroundColor = '#212C42';
+    output.style.backgroundColor = '#212C42';
+
+    Array.from(number_button).forEach(button =>{
+      button.style.color = 'white';
+    })
+
+    Array.from(operator_button).forEach(button =>{
+      button.style.color = '#10A985';
+    })
+
+    currentElementDisplay.style.color = '#10A985';
+    previousElementDisplay.style.color = '#10A985';
+
+    data_clear_all.style.color = '#10A985';
+    data_clear.style.color = '#10A985';
+    data_equals.style.color = '#10A985';
+    sign_button.style.color = '#10A985';
+    data_point.style.color = 'white';
+
+    check_label.style.backgroundColor = '#5D6E99';
+
+  }else if(checkbox.checked == false){
+    main.style.backgroundColor = 'white';
+    main_top.style.backgroundColor = '#f4fdfc';
+    output.style.backgroundColor = '#f4fdfc';
+    Array.from(number_button).forEach(button =>{
+      button.style.color = 'black';
+    })
+
+    Array.from(operator_button).forEach(button =>{
+      button.style.color = 'black';
+    })
+
+    currentElementDisplay.style.color = 'black';
+    previousElementDisplay.style.color = 'black';
+
+    data_clear_all.style.color = 'black';
+    data_clear.style.color = 'black';
+    data_equals.style.color = 'black';
+    sign_button.style.color = 'black';
+    data_point.style.color = 'black';
+
+    check_label.style.backgroundColor = 'transparent';
+  }
+})
 
 function appendNumber(number) {
   currentOperand += number.toString();
@@ -76,5 +148,10 @@ function calculate() {
     case "/":
       currentOperand = parseFloat(previousOperand) / parseFloat(currentOperand);
       break;
+    case "%":
+      currentOperand = parseFloat(previousOperand) % parseFloat(currentOperand);
+      break;
   }
 }
+
+
